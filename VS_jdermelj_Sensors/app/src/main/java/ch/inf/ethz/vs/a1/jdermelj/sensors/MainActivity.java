b1f13c73937c006df25cbcffe3effd7e174c32b9
wipascal <wipascal@student.ethz.ch>
2015-10-05 08:58:53 +0200

    sensors projekt erstellt

---
+++ VS_jdermelj_Sensors/app/src/main/java/ch/inf/ethz/vs/a1/jdermelj/sensors/MainActivity.java
@@ -0,0 +1,13 @@
+package ch.inf.ethz.vs.a1.jdermelj.sensors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
