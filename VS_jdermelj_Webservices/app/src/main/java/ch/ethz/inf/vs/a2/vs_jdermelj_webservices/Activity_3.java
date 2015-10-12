64a59fcdeae49ddbb6669a159a99940c50b81807
Andres <Andres@Andress-MacBook-Air.local>
2015-10-12 11:17:05 +0200

    Uploaded Ass 2
    hahah: ASS :’D hihihi

---
+++ VS_jdermelj_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/vs_jdermelj_webservices/Activity_3.java
@@ -0,0 +1,33 @@
+package ch.ethz.inf.vs.a2.vs_jdermelj_webservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Activity_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_3, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
