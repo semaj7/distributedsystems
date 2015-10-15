b5acda73defff9b91ef3ea8e580e58e2b9a40c59
wipascal <wipascal@student.ethz.ch>
2015-10-15 13:30:59 +0200

    höhö

--- VS_jdermelj_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/vs_jdermelj_webservices/ServerService.java
+++ VS_jdermelj_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/vs_jdermelj_webservices/ServerService.java
@@ -6,6 +6,7 @@ import android.os.IBinder;
 import android.util.Log;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.SocketException;
@@ -50,16 +51,44 @@ public class ServerService extends Service {
             Log.d("debugging",elem.getDisplayName());
 
             if(elem.getDisplayName().equals("wlan0")) {
-                wlan=elem;
+                wlan = elem;
                 Log.d("debugging", "got a wlan! :D");
                 break;
             }
-
-
         }
+
+
+
+        Enumeration<InetAddress> inetAddresses=wlan.getInetAddresses();
+        while(inetAddresses.hasMoreElements()){
+            inetAddresses.nextElement().getAddress();
+        }
+        /*
+        socket.b
+        socket.accept()
+        while(true){
+            socket.
+        }
+        */
         /*
         Log.d("ServerService", String.valueOf(networkInterfaces.nextElement().toString()));
         */
+    }
+
+    // GETS THE IP ADDRESS OF YOUR PHONE'S NETWORK
+    private InetAddress getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) { return inetAddress; }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("ServerActivity", ex.toString());
+        }
+        return null;
     }
 
 }
