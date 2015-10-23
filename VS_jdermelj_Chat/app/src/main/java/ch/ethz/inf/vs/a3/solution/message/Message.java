22eece283cc460e043194a68dcfa80a7f6b8e715
semaj7 <Jimmy@public-docking-cx-2431.ethz.ch>
2015-10-23 16:19:41 +0200

    naja
    cha d app nonid generiere.. git no e error, sorry
    i schaffe ersch am sunntig wider dranne

---
+++ VS_jdermelj_Chat/app/src/main/java/ch/ethz/inf/vs/a3/solution/message/Message.java
@@ -0,0 +1,14 @@
+package ch.ethz.inf.vs.a3.solution.message;
+
+/**
+ * Created by Jimmy on 23/10/15
+ *
+ * (because in MessageComparaor it sais: import ...solution.message.Message;)
+ *
+ * not sure if this is right or if i should just change the import in MessageComparator
+ */
+public class Message {
+
+
+
+}
