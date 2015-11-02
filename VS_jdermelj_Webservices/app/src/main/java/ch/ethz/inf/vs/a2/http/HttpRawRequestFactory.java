e468b0f1f388cd62f5ab08277fce7943e3f5bd23
Andres <Andres@public-docking-cx-3682.ethz.ch>
2015-11-02 16:08:31 +0100

    Took down old Project Files

--- VS_jdermelj_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/http/HttpRawRequestFactory.java
+++
@@ -1,9 +0,0 @@
-package ch.ethz.inf.vs.a2.http;
-
-
-public class HttpRawRequestFactory {
-	public static HttpRawRequest getInstance(String host, int port, String path) {
-		// return HttpRawRequest implementation
-		return new HttpRawRequestImpl(host, port, path);
-	}
-}
