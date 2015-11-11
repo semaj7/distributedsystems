b94576cac66e47d10f15327e0f7ecadaab3cff1e
wipascal <wipascal@student.ethz.ch>
2015-11-11 12:05:50 +0100

    hoppla

---
+++ FunWithFlags/app/src/main/java/ch/ethz/inf/vs/a4/funwithflags/MapsActivity.java
@@ -0,0 +1,311 @@
+package ch.ethz.inf.vs.a4.funwithflags;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.util.Random;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //just testing the getFlags(). should print a flag in the terminal
+        getFlags();
+
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    public boolean isLoggedIn() {
+
+        //TODO: implement this
+
+        return true;
+    }
+
+    public void setNewFlagClick(View v){
+
+        if (isLoggedIn()) {
+
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+            alert.setTitle(R.string.newFlagDialogTitle);
+            alert.setMessage(R.string.newFlagDialogMessage);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = input.getText().toString();
+                    // Do something with value!
+
+                    //adding a yellow marker at current location
+                    mMap.addMarker(new MarkerOptions().position(getCoordinates()).title(value).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+                }
+            });
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+
+        }
+
+        else {
+
+            switchToLogin();
+        }
+
+    }
+
+    public LatLng getCoordinates() {
+
+        double lon;
+        double lat;
+
+        //TODO: change to something meaningful
+
+
+        GPSTracker gps = new GPSTracker(this);
+        if(gps.canGetLocation()){
+
+            //TODO: this might also use outdated coordinates, check if they're old
+            lat = gps.getLatitude(); // returns latitude
+            lon = gps.getLongitude();
+        }
+
+        else {
+            gps.showSettingsAlert();
+            lat = RandomFloat( -90, 90 );
+            lon = RandomFloat( -180, 180 );
+        }
+
+        Log.d("DEBUG", String.valueOf(lat));
+        System.out.println("Lat:" + lat);
+        System.out.println("Lon:" + lon);
+
+        Toast.makeText(this, "Set flag at Lat:" + lat + "Lon:" + lon, Toast.LENGTH_LONG).show();
+
+        return new LatLng(lat, lon);
+    }
+
+    float RandomFloat(int a, int b) {
+        Random random = new Random(System.currentTimeMillis());
+        double floatval = Math.random();
+
+        System.out.println("Random float:" + floatval);
+
+        double rand = (floatval * (b - a)) + a;
+
+        System.out.println("Random result:" + rand);
+
+        return (float) rand;
+    }
+
+    public void switchToLogin() {
+
+        Intent newIntent = new Intent(this, LoginActivity.class);
+        startActivity(newIntent);
+
+    }
+
+    public void switchToProfile(View v) {
+
+        Intent newIntent = new Intent(this, ProfileActivity.class);
+        startActivity(newIntent);
+
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        //see other marker options: https://developers.google.com/maps/documentation/android-api/marker
+
+        //LatLng in degrees, (double, double), ([-90,90],[-180,180])
+        mMap.addMarker(new MarkerOptions().position(new LatLng(47, 8)).title("Penis"));
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(47.22, 8.33)).title("Höhö").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+    }
+
+    //PASCAL: ke ahnig wo dir dää code weit, aber i tues iz mau da ine.
+    void getFlags(){
+
+        /*
+        String id = editTextId.getText().toString().trim();
+        if (id.equals("")) {
+            Toast.makeText(this, "Please enter an id", Toast.LENGTH_LONG).show();
+            return;
+        }
+        loading = ProgressDialog.show(this,"Please wait...","Fetching...",false,false);
+        */
+
+        //String url = Config.DATA_URL+editTextId.getText().toString().trim();
+        String url="http://hochschultage.ch/getFlags.php";
+
+        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //loading.dismiss();
+                String flagID="";
+                String userName="";
+                String gpsCoordinates = "";
+                String categoryName = "";
+                String date = "";
+                String content = "";
+
+                try {
+
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray result = jsonObject.getJSONArray("result");
+                    JSONObject flagData = result.getJSONObject(0);
+                    flagID=flagData.getString("flagID");
+                    userName=flagData.getString("userName");
+                    gpsCoordinates=flagData.getString("gpsCoordinates");
+                    categoryName=flagData.getString("categoryName");
+                    date=flagData.getString("date");
+                    content=flagData.getString("content");
+
+                    Log.d("debug", "got flag with flagID "+flagID);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                //textViewResult.setText("Name:\t" + name + "\nAddress:\t" + address + "\nVice Chancellor:\t" + vc);
+                //showJSON(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MapsActivity.this,error.getMessage().toString(),Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+/*
+        String result = "";
+
+        //http post
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://example.com/getAllPeopleBornAfter.php");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+        }catch(Exception e){
+            Log.e("log_tag", "Error in http connection "+e.toString());
+        }
+        //convert response to string
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+
+            result=sb.toString();
+        }catch(Exception e){
+            Log.e("log_tag", "Error converting result "+e.toString());
+        }
+
+        //parse json data
+        try{
+            JSONArray jArray = new JSONArray(result);
+            for(int i=0;i<jArray.length();i++){
+                JSONObject json_data = jArray.getJSONObject(i);
+                Log.i("log_tag","id: "+json_data.getInt("id")+
+                                ", name: "+json_data.getString("name")+
+                                ", sex: "+json_data.getInt("sex")+
+                                ", birthyear: "+json_data.getInt("birthyear")
+                );
+            }
+        }
+        catch(JSONException e){
+            Log.e("log_tag", "Error parsing data "+e.toString());
+        }
+        */
+    }
+
+
+}
---
+++ getFlags.php
@@ -0,0 +1,57 @@
+<?php
+/*
+mysql_connect("localhost","distsys","intensiviLerngruppe");
+mysql_select_db("distsys");
+ 
+$q=mysql_query("SELECT * FROM Flags");
+while($e=mysql_fetch_assoc($q))
+        $output[]=$e;
+ 
+print(json_encode($output));
+ 
+mysql_close();
+*/
+
+
+//could put this stuff in a separate file
+define('HOST','localhost');
+define('USER','distsys');
+define('PASS','intensiviLerngruppe');
+define('DB','distsys');
+
+$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+
+
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+	
+	//$id  = $_GET['id'];
+	
+	//if we had the "defines" in another file we would need this
+	//require_once('dbConnect.php');
+	
+	$sql = "SELECT * FROM Flags WHERE flagID='1'";
+	
+	$r = mysqli_query($con,$sql);
+	
+	$res = mysqli_fetch_array($r);
+	
+	$result = array();
+	
+	array_push($result,array(
+		"flagID"=>$res['flagID'],
+		"userName"=>$res['userName'],
+		"gpsCoordinates"=>$res['gpsCoordinates'],
+		"categoryName"=>$res['categoryName'],
+		"date"=>$res['date'],
+		"content"=>$res['content']
+		)
+	);
+	
+	echo json_encode(array("result"=>$result));
+	
+	mysqli_close($con);
+		
+}
+
+?>
